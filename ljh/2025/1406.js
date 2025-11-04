e9c551a8088830c4b805796cc7139733939fe0d3
bebusl <bebus1998@naver.com>
2025-11-04 10:58:07 +0900

    ljh: 1406 editor_linkedlist

--- ljh/2025/1406.js
+++ ljh/2025/1406.js
@@ -5,35 +5,6 @@ const [initialValue, instructionLength, ...instructions] = require("fs")
   .trim()
   .split("\n");
 
-let editor = initialValue;
-let cursor = editor.length;
-
-// for (let i = 0; i < Number(instructionLength); i++) {
-//   console.log(editor);
-//   switch (instructions[i][0]) {
-//     case "L":
-//       if (cursor > 1) cursor -= 1;
-//       break;
-//     case "D":
-//       if (cursor < editor.length) cursor += 1;
-//       break;
-//     case "B":
-//       editor = editor.substring(0, cursor) + editor.substring(cursor);
-//       if (cursor > 1) cursor -= 1;
-//       break;
-//     case "P":
-//       const [, newChar] = instructions[i].split(" ");
-//       cursor += 1;
-//       let tmp = Array.from(editor);
-//       tmp.splice(cursor, 0, newChar);
-//       editor = tmp.join("");
-//       //   console.log("뽀뀨", instructions[i], newChar, Array.from(editor).splice
-//       break;
-//   }
-// }
-// console.log(":", editor);
-// // console.log("editor", editor);
-
 class Node {
   constructor(value) {
     this.value = value;
@@ -45,68 +16,65 @@ class Node {
 class LinkedList {
   constructor(node) {
     this.head = node;
+    this.tail = node; // tail 포인터 추가
     this.length = !!node ? 1 : 0;
   }
 
   append(newNode) {
     if (!this.head) {
       this.head = newNode;
+      this.tail = newNode;
     } else {
-      // 하나라도 있는경우
-      // 마지막 노드를 찾아서 ( next가 null일 때까지) 그 노드의 next로 이 노드를 넣는다.
-      let cur = this.head;
-      while (cur.next) {
-        cur = cur.next;
-      }
-
-      newNode.prev = cur;
-      cur.next = newNode;
+      // tail을 활용해서 O(1)로 추가
+      newNode.prev = this.tail;
+      this.tail.next = newNode;
+      this.tail = newNode;
     }
 
     this.length += 1;
   }
 
+  // targetNode 뒤에 newNode 삽입 (노드 레퍼런스를 직접 사용)
   insert(targetNode, newNode) {
-    if (!this.head) return;
+    const nextNode = targetNode.next;
 
-    let cursor = this.head;
-    while (cursor && cursor.value !== targetNode.value) {
-      cursor = cursor.next;
+    targetNode.next = newNode;
+    newNode.prev = targetNode;
+    newNode.next = nextNode;
+    if (nextNode) {
+      nextNode.prev = newNode;
+    } else {
+      // 맨 끝에 삽입하는 경우 tail 업데이트
+      this.tail = newNode;
     }
 
-    if (cursor) {
-      const nextNode = cursor.next;
-
-      cursor.next = newNode;
-      newNode.next = nextNode;
-    }
+    this.length += 1;
   }
 
-  delete(value) {
-    if (!this.head) return;
+  // 노드를 직접 삭제 (노드 레퍼런스를 직접 사용)
+  delete(node) {
+    if (!node) return;
 
-    if (this.head.value === value) {
-      this.head = this.head.next;
+    if (node === this.head) {
+      this.head = node.next;
       if (this.head) {
-        // ← 이 체크 추가!
         this.head.prev = null;
+      } else {
+        // 리스트가 비어버린 경우
+        this.tail = null;
       }
     } else {
-      let cur = this.head; // ← let 추가!
-      while (cur && cur.value !== value) {
-        // ← cur && 추가!
-        cur = cur.next;
+      const prevNode = node.prev;
+      const nextNode = node.next;
+
+      if (prevNode) {
+        prevNode.next = nextNode;
       }
-
-      if (!cur) return;
-
-      let prevNode = cur.prev;
-      let nextNode = cur.next;
-
-      prevNode.next = nextNode;
       if (nextNode) {
-        // ← 이 체크 추가!
         nextNode.prev = prevNode;
+      } else {
+        // 맨 끝 노드를 삭제하는 경우 tail 업데이트
+        this.tail = prevNode;
       }
     }
     this.length -= 1;
@@ -116,34 +84,70 @@ class LinkedList {
 const linkedEditor = new LinkedList();
 let curNode = null;
 
-Array.from(initialValue).forEach((element) => {
-  const newNode = new Node(element);
+// 초기화: tail을 활용한 append로 O(N)
+Array.from(initialValue).forEach((char) => {
+  const newNode = new Node(char);
   linkedEditor.append(newNode);
-  curNode = newNode;
+  curNode = newNode; // 커서를 맨 뒤로
 });
 
 for (let i = 0; i < Number(instructionLength); i++) {
   switch (instructions[i][0]) {
     case "L":
-      if (curNode.prev) curNode = curNode.prev;
+      // 커서를 왼쪽으로: curNode를 이전 노드로 이동
+      if (curNode) {
+        curNode = curNode.prev; // prev가 null이면 맨 앞으로 이동
+      }
       break;
     case "D":
-      if (curNode.next) curNode = curNode.next;
+      // 커서를 오른쪽으로: curNode를 다음 노드로 이동
+      if (curNode === null) {
+        // 커서가 맨 앞일 때는 head로 이동
+        curNode = linkedEditor.head;
+      } else if (curNode.next) {
+        curNode = curNode.next;
+      }
       break;
     case "B":
-      linkedEditor.delete(curNode.value);
+      // 커서 왼쪽 문자 삭제: curNode를 삭제하고 이전 노드로 이동
+      if (curNode) {
+        const prevNode = curNode.prev;
+        linkedEditor.delete(curNode);
+        curNode = prevNode; // 이전 노드로 이동 (null이 될 수도 있음)
+      }
       break;
     case "P":
+      // 커서 왼쪽에 문자 추가
       const [, newValue] = instructions[i].split(" ");
-      linkedEditor.insert(curNode, new Node(newValue));
+      const newNode = new Node(newValue);
+
+      if (curNode === null) {
+        // 커서가 맨 앞에 있을 때
+        if (linkedEditor.head) {
+          newNode.next = linkedEditor.head;
+          linkedEditor.head.prev = newNode;
+          linkedEditor.head = newNode;
+        } else {
+          // 빈 리스트일 때
+          linkedEditor.head = newNode;
+          linkedEditor.tail = newNode;
+        }
+        linkedEditor.length += 1;
+        curNode = newNode;
+      } else {
+        linkedEditor.insert(curNode, newNode);
+        curNode = newNode; // 새로 삽입한 노드로 이동
+      }
       break;
   }
 }
 
+let result = [];
 let _ = linkedEditor.head;
-while (_.next) {
-  console.log(_.value);
+while (_) {
+  result.push(_.value);
   _ = _.next;
 }
+console.log(result.join(""));
 
 //이거 또 설계안하고 해서 그렇구만.
