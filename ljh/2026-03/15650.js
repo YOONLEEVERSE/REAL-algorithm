7a946a25d7611c89f9757b9e9e90d9fc32a8f869
bebusl <bebus1998@naver.com>
2026-04-02 21:20:53 +0900

    ljh: 백준, 15650

---
+++ ljh/2026-03/15650.js
@@ -0,0 +1,19 @@
+const fs = require("fs");
+const [N, M] = fs.readFileSync(0).toString().trim().split(" ").map(Number);
+
+// function getCombination(items) {
+//   let result = [];
+
+// for(let j = items)
+
+//   return result;
+// }
+
+// let tmp = Array.from({ legnth: N }, (_, idx) => idx + 1);
+// for (let i = 2; i <= M; i++) {
+//   tmp = getCombination(tmp);
+// }
+
+// // 어우 이것도 부
+
+// 모르겟당. ㅎㅎ 다시풀어봐야징 ~! 
